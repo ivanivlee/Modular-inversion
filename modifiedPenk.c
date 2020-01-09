fe6ea0b86a451b898d5bf4fc34c529306c4fbcbb
ivanivlee <ivanatrummova@gmail.com>
2020-01-09 10:32:25 +0100

    input checks on modified penks algorithm

--- modifiedPenk.c
+++ modifiedPenk.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
-int main()
+#include<stdlib.h>
+
+int inputCountCheck(int argCount)    //checks number of arguments 
 {
-    printf("Hello, this is the new modular inverse algorithm \n");
+    if (argCount > 3)   
+    {
+        printf("Too many arguments. \n");
+        return 1;
+    }
+    if (argCount < 3)   
+    {
+        printf("Too few arguments. \n");
+        return 1;
+    }
+    return 0;
+}
+
+int inputSizeCheck(int a, int p)    //checks whether 1 <= a <= p or not
+{
+     if (a < 1)
+    {
+        printf("Integer a is too small. \n");
+        return 1;
+    }
+    if (a > p)
+    {
+        printf("Integer a is too big. \n");
+        return 1;
+    }
+    return 0;
+}
+
+// in: integer a, prime p, 1 <= a <= p - two parametres from terminal
+int main(int argc, char* argv[])
+{
+    //check and prepare input
+    if (inputCountCheck(argc) == 1){return 1;}
+    int a = atoi(argv[1]); // convert to int
+    int p = atoi(argv[2]); 
+    if (inputSizeCheck(a, p) == 1){return 1;}
+    
+    printf("Integer a = %s \n", argv[1]);
+    printf("Prime p = %s \n", argv[2]);
+    
+    
+    
     return 0;
 }
