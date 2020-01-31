872147c5783922f929f78376393e2ab1032728f4
ivanivlee <ivanatrummova@gmail.com>
2020-01-31 10:12:53 +0100

    modified Penk algorithm - basic version created

--- modifiedPenk.c
+++ modifiedPenk.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include<stdlib.h>
 
+struct Tuple {
+    int a;
+    int b;
+}; //for returning two integers from functions
+
 // variables and data types
 unsigned int a, p; //from input
 
@@ -47,7 +52,7 @@ char *binbin(int n)     //display binary
     return(bin);
 }
 
-void binarySandBox (int a, int p)
+void binarySandBox (int a, int p) // this is a function with a fex binary operations, just for test and inspiration. to be erased.
 {
     printf("Integer a = %d \n", a);
     printf("Prime p = %d \n", p);
@@ -76,19 +81,85 @@ void binarySandBox (int a, int p)
 }
 
 
-int modifiedPenk(int a, int p)
+int modifiedPenk(int a, int p) //the real algorithm is in this function
 {
-    int u = p;
-    int v = a;
+    int u = p; //init - line 1
+    int v = a; 
     int r = 0;
     int s = 1;
-    int k = 0;
-    while (v > 0)
+    int k = 0; //init counter - line 2
+    int x = 0; //difference?
+    while (v > 0) //while loop - line 3
     {
-        
-        
+        if (u % 2 == 0) //is this really the way to test the parity? what about checking the last bit? Does C do it like this?
+        {
+            if (r % 2 == 0) //r is even - line 5
+            {
+                u = u>>1; //halving - right shift
+                r = r>>1; 
+                k++; //line 6
+                printf("u = %d r = %d k = %d \n", u, r, k);
+            }
+            else
+            {
+                u = u>>1; //halving - right shift
+                r = (r + p)>>1; 
+                k++; //line 8
+                printf("u = %d r = %d k = %d \n", u, r, k);
+            }
+        }
+        else if (v % 2 == 0)
+        {
+            if (s % 2 == 0)
+            {
+                v = v>>1; //halving - right shift
+                s = s>>1; 
+                k++; //line 11
+                printf("v = %d s = %d k = %d \n", v, s, k);
+            }
+            else
+            {
+                v = v>>1; //halving - right shift
+                s = (s + p)>>1; 
+                k++; //line 11
+                printf("v = %d s = %d k = %d \n", v, s, k);
+            }
+            
+        }
+        else
+        {
+            x = (u - v); //line 14
+            if (x > 0)  
+            {
+                u = x;
+                r = r - s;
+                if (r < 0)
+                {
+                    r = r + p;
+                }
+            }
+            else
+            {
+                v = -x;
+                s = s - r;
+                if (s < 0)
+                {
+                    s = s + p; //line 22
+                }
+                
+            }
+            
+        }    
     }
-
+    if (r > p)
+    {
+        r = r - p; //line 24
+    }
+    if (r < 0)
+    {
+        r = r + p;
+    }
+    printf("Result r = %d, k = %d \n", r, k); //line 27
     return 0;
 }
 // in: integer a, prime p, 1 <= a <= p - two parametres from terminal
@@ -100,9 +171,10 @@ int main(int argc, char* argv[])
     p = atoi(argv[2]); 
     if (inputSizeCheck(a, p) == 1){return 1;}
     
-    //binarySandBox(a, p);
+    //binarySandBox(a, p); //just playing with binary stuff, to be erased
 
-    modifiedPenk(a, p);
+    modifiedPenk(a, p); //THEREALALGORITHM
+
 
     
     return 0;
