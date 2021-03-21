6ba06c77e30200d11efed212f543595c211b0baa
Ivanivlee <ivanatrummova@gmail.com>
2021-03-21 11:54:20 +0100

    try to run this elsewhere

--- taowu compare/LS.c
+++ taowu compare/LS.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include<stdlib.h>
-//#include <math.h> 
 
 typedef signed short NUM; 
-NUM a, p; //from input
-//NUM result, halvings; //to store the result and counter of halvings
+NUM a, p; //input - element a, prime p
 
 int inputCountCheck(int argCount)    //checks number of arguments 
 {
@@ -55,7 +53,7 @@ int sizeOfTheBiggerNumber(NUM u, NUM v)
     } 
     return count; 
 } 
-int bit(NUM n, int bit)
+int bit(NUM n, int bit) //value of nth MSB bit
 {
         n = (n >> bit);
         if (n % 2 == 0)
@@ -83,19 +81,15 @@ int bits(NUM n, int bit) //how many bits 0, 1, ... n are still zeros - valuation
         //printf("0");
         return 0;
 }
-int TW(NUM a, NUM p) //the real algorithm is in this function 
+int LS(NUM a , NUM p)
 {
-    //Phase 1
     NUM u = p;
     NUM v = a;
     NUM r = 0;
-    NUM s = 1;   //init
-    int cu = 0;
-    int cv = 0;  //counters of left shifts
+    NUM s = 1;
     NUM Rmu = 1; //2^cu
     NUM Rmv = 1; //2^cv
-    int size = sizeOfTheBiggerNumber(u,v); // should be flexible by the larger number
-    int i = 0;
+    NUM size = sizeOfTheBiggerNumber(u,v);
 
     //counters
     int add = 0;
@@ -107,10 +101,10 @@ int TW(NUM a, NUM p) //the real algorithm is in this function
     int fbr = 0;  //first branch
     int sbr = 0;  //second branch
     int tbr = 0;  //third branch
-        
+    
 
     while (((u^Rmu) != 0) && ((u+Rmu) != 0) && ((v^Rmv) != 0) && ((v+Rmv) != 0))
-    {   
+    {
         loop++;
         /*printf("u = %d, ", u); bin(u, size);
         printf("v = %d, ", v); bin(v, size);
@@ -121,42 +115,22 @@ int TW(NUM a, NUM p) //the real algorithm is in this function
         if ((bit(u, size) == 0 && bit(u, size-1) == 0)|| ((bit(u, size) == 1 && bit(u, size-1) == 1) && (bits(u, size-2) == 1)))
         {
             fbr++; //entered the first branch
-            u = (u<<1); shift++; //left shift is performed right away
+            u = (u<<1); shift++;
+            if (Rmu >= Rmv)   { r = (r<<1); shift++;} else { s = (s>>1); shift++;}
             Rmu = (Rmu<<1); 
-            cu++; //counter
-            even++; //if s is even
-            if (s%2 == 0) 
-            {
-                s = (s>>1); shift++;
-            }
-            else
-            {
-                s = s + p; add++; //first correction - we need to divide even number by two
-                s = (s>>1); shift++;
-            }
         }
         else if ((bit(v, size) == 0 && bit(v, size-1) == 0)|| ((bit(v, size) == 1 && bit(v, size-1) == 1) && (bits(v, size-2) == 1)))
         {
             sbr++; //entered the second branch
             v = (v<<1); shift++;
-            Rmv = (Rmv<<1); 
-            cv++;
-            even++;
-            if (r%2 == 0)
-            {
-                r = (r>>1); shift++;
-            }
-            else
-            {
-                r = r + p; add++;
-                r = (r>>1); shift++;
-            }
+            if (Rmv >= Rmu)   { s = (s<<1); shift++;} else { r = (r>>1); shift++;}
+            Rmv = (Rmv<<1); shift++;
         }
         else
         {
             tbr++; //entered the third branch
             NUM oper; //0 is minus, 1 is plus
-            if (bit(u, size) == bit(v, size))//check if the signs are the same
+            if (bit(u, size) == bit(v, size)) //check if the signs are the same
             {
                 oper = 0; //minus
                 sub++;sub++; //two subtractions will happen
@@ -167,33 +141,22 @@ int TW(NUM a, NUM p) //the real algorithm is in this function
                 add++;add++; //two additions will happen
             }
             sub++; //comparison will be performed by subtraction
-            if (cu <= cv)
+            if (Rmu <= Rmv)
             {
                 u = (oper == 0) ? (u - v) : (u + v);
                 r = (oper == 0) ? (r - s) : (r + s);
-                sub++; //comparison
-                if (r > p) { r = r - p; sub++; } 
             }
             else
             {
                 v = (oper == 0) ? (v - u) : (v + u);
                 s = (oper == 0) ? (s - r) : (s + r);
-                sub++;
-                if (s > p) { s = s - p; sub++;} 
             }
-        }   
+        } 
     }
-
     if (((v^Rmv) == 0) || ((v+Rmv) == 0))
     {
         r = s;
-        NUM vnShifted = (v>>(size-2)); //the value is (0, 0, ..., vn)
-        NUM vnCorrect = (vnShifted<<(size - 2)); //the value is (vn, 0, 0, ..., 0)
-        NUM unShifted = (u>>(size - 2)); //the value is (0, 0, ..., un)
-        NUM unCorrect = (unShifted<<(size - 2)); //the value is (un, 0, 0, ..., 0)
-        u = u^unCorrect^vnCorrect; //the result is un := vn
-        //u = (bit(v, size)<<(size-1)) + bits(u, size-1);
-        cv = cu;
+        u = v;
     }
     if (u < 0)
     {
@@ -204,39 +167,22 @@ int TW(NUM a, NUM p) //the real algorithm is in this function
         else
         {
             r = p - r;
-        }  
+        }
     }
     if (r < 0)
     {
-        r = r + p; 
+        r = r + p;  
     }
-    
-    for (size_t i = 1; i < (cv + 1); i++)
-    {
-        r = (r<<1); shift++;
-        if (r >= p)
-        {
-            r = (r - p); 
-            sub++;
-        }
-        if (r < 0)
-        {
-            r = r + p; 
-            add++;
-        }
-    } 
-    
+    //printf("%d * %d = %d mod %d", a, r, (a*r)%p, p);
     
     FILE *fp;
-    fp = fopen("TW.txt", "a");
+    fp = fopen("LS.txt", "a");
     fprintf(fp, "%d %d %d %d %d %d %d %d %d %d %d %d %d\n", a, r, p, (r*a)%p, add ,sub, shift, even, poz, loop, fbr, sbr, tbr);
     fclose(fp);
-    return 0; 
+    return 0;
 }
 
 
-
-// in: integer a, prime p, 1 <= a <= p - two parametres from terminal
 int main(int argc, char* argv[])
 {
     //check and prepare input
@@ -245,7 +191,7 @@ int main(int argc, char* argv[])
     p = atoi(argv[2]); 
     if (inputSizeCheck(a, p) == 1){return 1;}
     
-    TW( a, p);
-    
+    LS(a, p);
+
     return 0;
 }
